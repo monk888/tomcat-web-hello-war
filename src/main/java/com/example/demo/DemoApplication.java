bbeffeca6c706fefc7ecb1b7bdf3703bea7e9f13
monk <you@example.com>
2019-07-14 09:42:08 +0800

    update

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//@EnableApolloConfig
+//@SpringBootApplication
+//public class DemoApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(DemoApplication.class, args);
+//    }
+//
+//}
+//@EnableApolloConfig
+@SpringBootApplication
+public class DemoApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
+}
---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+//    @Value("${ip:ip}")
+//    private String ip;
+//    @Value("${port:port}")
+//    private String port;
+//    @Value("${serverName:serverName}")
+//    private String serverName;
+//    @Value("${chen:chen}")
+//    private String chen;
+
+
+    @RequestMapping("/test")
+    public String apollo() {
+        return "20190714  what's your problem!";
+    }
+}
